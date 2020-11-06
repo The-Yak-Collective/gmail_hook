1e37784cb4c44463d7028786f3e4525c73aca556
tilnow <44685516+tilnow@users.noreply.github.com>
2020-11-06 14:52:35 +0200

    Update gmailcalv1.py

--- gmailcalv1.py
+++ gmailcalv1.py
@@ -36,6 +36,7 @@ async def on_ready():
     payload = {'content':json.dumps(reminders)}
     headers = {'content-type': 'application/json'}
     r = requests.post(url, data=payload, headers=headers)
+    print(r)
     
     
 def main():
