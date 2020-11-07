402b7e1b4f186093b35e49851f8e359d0e7d6f4c
tilnow <44685516+tilnow@users.noreply.github.com>
2020-11-07 18:54:09 +0200

    move to ical

--- gmailcalv1.py
+++ gmailcalv1.py
@@ -1,4 +1,5 @@
 
+import icalendar as ical
 import base64
 import pickle
 import requests
@@ -25,38 +26,13 @@ reminders=[]
 
 @client.event
 async def on_ready(): 
-
+    print('should never reach here, as decide dto use dicord webhook instead')
     print('We have logged in as {0.user}'.format(client),  client.guilds)
-    print('going to send:',[x for x in reminders], 'to:',704047116086935602)
     channel = client.get_channel(704047116086935602)#calendar events channel
     #await channel.send('test message')
     #better - do not connect to discord at all. just use ADY_WEBHOOK using https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks and https://discord.com/developers/docs/resources/webhook
     #here is example of posting to test server
-    url = os.getenv('TEST_HOOK')
-    for x in reminders:
-        y="Heads up! @here "+x[0]+'\n\n'#+x[1]
-        ttlpos=x[1].find('\nTitle:')
-        nlat=x[1].find('\n',ttlpos+1)
-        ttl=x[1][ttlpos+7:nlat]
-        y=y+"\ntitle is maybe:"+ttl
-        whenpos=x[1].find('\nWhen:')
-        nlaw=x[1].find('\n',whenpos+1)
-        whn=x[1][whenpos+6:nlaw]
-        y=y+"\nwhen is maybe:"+whn
-        dtl=x[1][nlat+1:whenpos-1]
-        y=y+"\ndetails are maybe:"+dtl
-        
-        payload = {'content': y}
-        headers = {'content-type': 'application/json'}
-        r = requests.post(url, data=payload)#, headers=headers)
-        print(r,r.text)#,r.json())
-        newwhen=x[0][x[0].find('@')+1:]
-        rngs=newwhen.find('-')
-        rnge=newwhen.find('(',rngs)
-        newwhen=newwhen[:rngs-1]+' '+newwhen[rnge:rnge+5]
-        print('extracted time:',newwhen, 'need to convert 3 letter code to real offset; note it depend son machine locals AND pacific time has DST')
-        #t=datetime.strptime(newwhen,'%a %b %d, %Y %I%p %z')
-        #print('read time:',t)
+
     
     
 def main():
@@ -113,8 +89,37 @@ def main():
 
     request = {  'labelIds': ['INBOX'],  'topicName': 'projects/yc-cal-reminders-1604260822408/topics/hook' }
     print(service.users().watch(userId='me', body=request).execute())#needs to be renewed daily. or at least weekly. but we get enough reminders to make this happen on its own. we hope
-    discord_token=os.getenv('CAL_DISCORD_KEY')
-    client.run(discord_token)
+#    discord_token=os.getenv('CAL_DISCORD_KEY')
+#    client.run(discord_token)
+    print('going to send:',[x for x in reminders], 'to:',704047116086935602)
+    url = os.getenv('TEST_HOOK')
+    icalurl='https://calendar.google.com/calendar/ical/o995m43173bpslmhh49nmrp5i4%40group.calendar.google.com/public/basic.ics'
+    icalfile=requests.get(icalurl)
+    print(icalfile)
+    for x in reminders:
+        y="Heads up! @here "+x[0]+'\n\n'#+x[1]
+        ttlpos=x[1].find('\nTitle:')
+        nlat=x[1].find('\n',ttlpos+1)
+        ttl=x[1][ttlpos+7:nlat]
+        y=y+"\ntitle is maybe:"+ttl
+        whenpos=x[1].find('\nWhen:')
+        nlaw=x[1].find('\n',whenpos+1)
+        whn=x[1][whenpos+6:nlaw]
+        y=y+"\nwhen is maybe:"+whn
+        dtl=x[1][nlat+1:whenpos-1]
+        y=y+"\ndetails are maybe:"+dtl
+        
+        payload = {'content': y}
+        headers = {'content-type': 'application/json'}
+        r = requests.post(url, data=payload)#, headers=headers)
+        print(r,r.text)#,r.json())
+        newwhen=x[0][x[0].find('@')+1:]
+        rngs=newwhen.find('-')
+        rnge=newwhen.find('(',rngs)
+        newwhen=newwhen[:rngs-1]+' '+newwhen[rnge:rnge+5]
+        print('extracted time:',newwhen, 'need to convert 3 letter code to real offset; note it depend son machine locals AND pacific time has DST')
+        #t=datetime.strptime(newwhen,'%a %b %d, %Y %I%p %z')
+        #print('read time:',t)
 
 if __name__ == '__main__':
     main()
