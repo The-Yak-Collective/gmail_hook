b7f3e2429eb29e552c7e812b663f56a415dfc44f
tilnow <44685516+tilnow@users.noreply.github.com>
2020-11-07 20:05:27 +0200

    Update gmailcalv1.py

--- gmailcalv1.py
+++ gmailcalv1.py
@@ -100,7 +100,13 @@ def main():
     es=events(icalurl)
     #print('just the events:',es)
     for y in es:
-        print('time left for:', y.summary, y.time_left())
+        tl=y.time_left()
+        days=tl.days
+        hours=tl.seconds//3600
+        minutes=(tl.seconds-hours*3600)//60
+        ts=str(tl.days) + ' days'
+        ts=ts+ 'and '+ str(hours)+ ' hours' +' and '+str(minutes)+ ' minutes'
+        print('time left for:', y.summary, ts)
     for x in reminders:
         y="Heads up! @here "+x[0]+'\n\n'#+x[1]
         ttlpos=x[1].find('\nTitle:')
